0012efaaca57cabfb5765ebb6d0fae88e308bcb5
saix <sai.xu@citrix.com>
2017-10-12 10:56:43 +0800

    Initial Commit

---
+++ MySlackTextViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MySlackTextViewController
+//
+//  Created by saix on 2017/10/12.
+//  Copyright © 2017年 sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
